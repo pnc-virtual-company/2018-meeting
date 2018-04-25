975fc3fe603075977824c3fd1e6fb1c0d2f2cb24
PNC\Thintha.CHAN <thintha.chan@student.passerellenumeriques.org>
2018-04-25 13:57:16 +0700

    update content in left sidebar. "CHHUNHAK CHHOEUNG"

--- application/views/template_admin/left_sidebar.php
+++ application/views/template_admin/left_sidebar.php
@@ -13,7 +13,7 @@
     <!-- Sidebar Navigation Menus-->
     <div class="main-menu">
       <ul id="side-main-menu" class="side-menu list-unstyled">                  
-        <li><a href="<?php base_url(); ?>welcome/location"> <i class="mdi mdi-map"></i>Location</a></li>
+        <li><a href="<?php echo base_url(); ?>welcome/location"> <i class="mdi mdi-map"></i>Location</a></li>
         <li><a href="<?php echo base_url(); ?>welcome/list_room"> <i class="mdi mdi-home-heart"></i>List Room</a></li>
       </ul>
     </div>
