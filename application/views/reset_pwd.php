4fc2666a2de517cf18b13deb1ca8048bf5346226
PNC\Danet.THORNG <danet.thorng@student.passerellesnumeriques.org>
2018-04-25 09:18:48 +0700

    I have done with register and reset password.  "Danet-THRONG"

---
+++ application/views/reset_pwd.php
@@ -0,0 +1,25 @@
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-header">
+						<div class="card-title"><h3 style="text-align: center; color: green;">Reset Password</h3></div>
+					</div>
+					<div class="card-body">
+						<label>Old Password</label>
+						<input type="text" name="username" class="form-control" placeholder="Enter Your First Name"><br>
+						<label>New Password</label><br>
+						<input type="text" name="username" class="form-control" placeholder="Enter Your Last Name"><br>
+						<label>Confirm New Password</label><br>
+						<input type="text" name="username" id="user" class="form-control" placeholder="Enter Your Email Address"><br>
+					</div>
+					<div class="card-footer">
+						<button type="button" class="btn btn-outline-success float-right">Reset Now</button>
+						<button type="button" class="btn btn-outline-danger float-left">Cancel</button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3"></div>
+		</div>
+	</div>		
