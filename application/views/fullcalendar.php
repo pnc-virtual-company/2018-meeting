453c87c4fa9d60808c3c0b8a5bd942c20800966d
Chhunhak CHHOEUNG <chhunhak.chhoeung@gmail.com>
2018-04-26 20:07:10 +0700

    Create fullcalendar page. and add toggle menu on left sidebar. "Chhunhak CHHOEUNG"

---
+++ application/views/fullcalendar.php
@@ -0,0 +1,16 @@
+<style>
+	span{
+		all:none;
+	}
+</style>
+    <div class="container">
+        <div class="row">
+        	<div class="col-md-1"></div>
+        	<div class="col-md-10">
+        		<hr>
+	            	<div id="calendar" ></div>
+        	</div>
+        	<div class="col-md-1"></div>
+        </div>
+    </div>
+    
--- application/views/template_admin/left_sidebar.php
+++ application/views/template_admin/left_sidebar.php
@@ -9,6 +9,7 @@
         <h2 class="h5">Rady Y</h2>
       </div>
       <!-- Small Brand information, appears on minimized sidebar-->
+      <div class="sidenav-header-logo"><a href="index.html" class="brand-small text-center"> <strong>R</strong><strong class="text-primary">Y</strong></a></div>
     </div>
     <!-- Sidebar Navigation Menus-->
     <div class="main-menu">
@@ -25,7 +26,7 @@
     <nav class="navbar">
       <div class="container-fluid">
         <div class="navbar-holder d-flex align-items-center justify-content-between">
-          <div class="navbar-header"><a href="#" class="navbar-brand">
+          <div class="navbar-header"><a id="toggle-btn" href="#" class="menu-btn"><i class="mdi mdi-menu"> </i></a><a href="#" class="navbar-brand">
               <div class="brand-text d-none d-md-inline-block"><span>Meeting Room&nbsp;</span><strong class="text-primary">management</strong></div></a>
              <!--  <img id="profile-img" class="profile-img-card" src="<?php echo base_url(); ?>assets/images/logo_meeting.png" /> -->
           </div>
