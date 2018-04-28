90499f1c5a2f9b4da3d058604184635eb1f962db
Chhunhak CHHOEUNG <chhunhak.chhoeung@gmail.com>
2018-04-28 08:32:31 +0700

    Change name of left sidebar menu. and change controller. "Chhunhak CHHOEUNG"

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,7 +17,7 @@ class Welcome extends CI_Controller {
 		$this->load->view('list_location');
 		$this->load->view('template_admin/footer');
 	}
-	public function dashboard()
+	public function occupancyRate()
 	{
 		$this->load->view('template_admin/header');
 		$this->load->view('template_admin/left_sidebar');
