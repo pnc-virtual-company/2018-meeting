1fafd28c0ab36abb6e42cc88ac8a602a49628f41
Chan Thintha <thintha.chan07@gmail.com>
2018-05-04 16:38:52 +0700

    Change the title when hover on icon, change modal pop up. "Thintha"

--- application/views/list_room.php
+++ application/views/list_room.php
@@ -23,11 +23,11 @@
                 <td>
                   <?php echo $i;; ?>&nbsp;
                    <a href="<?php echo base_url(); ?>welcome/delete_room?$room_id=<?php echo $row->room_id; ?>" data-toggle="modal" data-target="#myModal" title="Delete this room"><i class="mdi mdi-delete"></i></a>
-                   <a href="<?php echo base_url(); ?>welcome/update_room?room_id=<?php echo $row->room_id; ?>" title="Update this room"><i class="mdi mdi-pencil"></i></a>
-                   <a href="<?php echo base_url(); ?>welcome/book_meeting?room_id=<?php echo $row->room_id; ?>" title="Book a room"><i class="mdi mdi-notebook"></i></a>            
-                   <a href="<?php echo base_url(); ?>welcome/fullCalendar" title="Calendar"><i class="mdi mdi-table-large"></i></a>            
-                   <a href="<?php echo base_url(); ?>welcome/room_availability?room_id=<?php echo $row->room_id; ?>" title="View room"><i class="mdi mdi-check"></i></a>        
-                  <a href="#" title="View location room" data-toggle="modal" data-target="#location_room"><i class="mdi mdi-source-commit-start"></i></a>
+                   <a href="<?php echo base_url(); ?>welcome/update_room?room_id=<?php echo $row->room_id; ?>" title="Update room information"><i class="mdi mdi-pencil"></i></a>
+                   <a href="<?php echo base_url(); ?>welcome/book_meeting?room_id=<?php echo $row->room_id; ?>" title="Make a reservation"><i class="mdi mdi-notebook"></i></a>            
+                   <a href="<?php echo base_url(); ?>welcome/fullCalendar" title="Room calendar"><i class="mdi mdi-table-large"></i></a>            
+                   <a href="<?php echo base_url(); ?>welcome/room_availability?room_id=<?php echo $row->room_id; ?>" title="Room availability"><i class="mdi mdi-check"></i></a>        
+                  <a href="#" title="View room location" data-toggle="modal" data-target="#location_room"><i class="mdi mdi-source-commit-start"></i></a>
                 </td>
                 <td><?php echo $row->room_name; ?></td>
                 <td><?php echo $row->firstname; ?></td>
@@ -40,53 +40,63 @@
      </tbody>        
      </table>
     </div>
-    <div class="row">
-      <div class="col-md-1"></div>
-      <div class="col-11">
-          <button class="btn btn-primary"><a href="<?php echo base_url(); ?>welcome/create_room?loc_id=<?php error_reporting(0); echo $row->loc_id; ?>" class="text-center">
+  
+
+<div class="row-fluid"><div class="col-12">&nbsp;</div></div>
+  <!-- button create location -->
+  <div class="container">
+    <div class="row-fluid">
+      <div class="col-12">
+        
+        &nbsp;
+        <button class="btn btn-primary"><a href="<?php echo base_url(); ?>welcome/create_room?loc_id=<?php error_reporting(0); echo $row->loc_id; ?>" class="text-center">
             <i style="color: #fff" class="mdi mdi-plus-circle" data-toggle="tooltip" title="Add new room"></i><span style="color: #fff">&nbsp; Creat a room</span>
           </a>
-          </button>
+          </button>&nbsp;
           <button class="btn btn-primary"><a href="#" class="text-center">
             <i style="color: #fff" class="mdi mdi-file-export" data-toggle="tooltip" title="Export the list of room"></i><span style="color: #fff">&nbsp; Export list</span>
           </a>
           </button>
       </div>
-    </div>
-</div>
-</div>
-
-
-
-<!-- Modol pop up delete -->
-  <div class="modal fade" id="myModal">
-  <div class="row">
-    
   </div>
-  <div class="modal-dialog">
+  </div>
+
+
+
+
+
+
+
+</div>
+</div>
+
+<!-- Delete modal pop up -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
-
-      <!-- Modal Header -->
       <div class="modal-header">
-        
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h5 class="modal-title">Delete confirmation</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-
-      <!-- Modal body -->
       <div class="modal-body">
-      <h4 class="modal-title">Are you sure to delete this room!</h4>
+          <p>You are going to delete this room.</p>
+          <p>Are you sure that you want to perform this action?</p>
       </div>
-
-      <!-- Modal footer -->
       <div class="modal-footer">
         <a href="<?php echo base_url(); ?>welcome/delete_room?room_id=<?php echo $row->room_id; ?>" class="btn btn-danger">OK </a>
         
         <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
       </div>
-
     </div>
   </div>
 </div>
+
+
+
+
+
 
 <!-- Modal pup up list location room -->
 <div class="modal fade" id="location_room">
