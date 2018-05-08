ccfc5ae9c74e1c8816131d73339be39770dadb3a
PNC\Samreth.SAROEURT <saroeurtsamreth@gmail.com>
2018-05-08 13:45:31 +0700

    no message

--- application/views/update_booking.php
+++ application/views/update_booking.php
@@ -11,7 +11,7 @@
 				<div class="form-group">
 					<label for="">Start Date</label>
 					<div class="input-group date" id="datetimepicker1" data-target-input="nearest">
-                    	<input type="text" value="<?php echo $rows->startDate; ?>" class="form-control datetimepicker-input" name="startDate" data-target="#datetimepicker1"/>
+                    	<input type="text" value="<?php echo $rows->startDate; ?>" class="form-control datetimepicker-input" name="startDate" data-toggle="datetimepicker" data-target="#datetimepicker1"/>
                     	<div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
                     	    <div class="input-group-text"><i class="mdi mdi-calendar-clock"></i></div>
                     	</div>
@@ -21,7 +21,7 @@
 				<div class="form-group">
 					<label for="">End Date</label>
 					<div class="input-group date" id="datetimepicker2" data-target-input="nearest">
-                    	<input type="text" value="<?php echo $rows->endDate; ?>" name="endDate" class="form-control datetimepicker-input" data-target="#datetimepicker2"/>
+                    	<input type="text" value="<?php echo $rows->endDate; ?>" name="endDate" class="form-control datetimepicker-input" data-toggle="datetimepicker" data-target="#datetimepicker2"/>
                     	<div class="input-group-append" data-target="#datetimepicker2" data-toggle="datetimepicker">
                     	    <div class="input-group-text"><i class="mdi mdi-calendar-clock"></i></div>
                     	</div>
@@ -34,7 +34,7 @@
 				<div class="form-group">
 					<div>
 						<button type="submit" class="btn btn-success">Update Booking</button>
-					    <a  class="btn btn-danger"  href="<?php echo base_url(); ?>Welcome/select_room_request" class="text-center">
+					    <a  class="btn btn-danger float-right"  href="<?php echo base_url(); ?>Welcome/select_room_request" class="text-center">
 					      <i style="color: #fff" class="mdi mdi-close" data-toggle="tooltip" title="Add new room"></i><span style="color: #fff">&nbsp; Cancel</span>
 					    </a>
 					</div>
