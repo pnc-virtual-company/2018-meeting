7873df6c9f0f95f4353f4d55be3b6121d08b9986
Chan-Thintha <thintha.chan07@gmail.com>
2018-05-09 17:00:20 +0700

    [GD016] change location
    refs [GD016]

--- application/views/create_location.php
+++ application/views/create_location.php
@@ -11,8 +11,8 @@
 <br>
 <div class="container">
     <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-md-4">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
         <div class="">
           <div class="text-center">
             <h3>Create A New Location</h3>
@@ -46,7 +46,7 @@
           
         </div>
       </div>
-      <div class="col-md-4"></div>
+      <div class="col-md-3"></div>
       
     </div>
     
