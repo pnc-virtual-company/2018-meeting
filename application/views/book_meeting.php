40c95bed82acec7fe4862e276de5d66fabcf182f
PNC\Danet.THORNG <danet.thorng@gmail.com>
2018-05-09 19:38:06 +0700

    I have change on form. "Danet-THORNG"

--- application/views/book_meeting.php
+++ application/views/book_meeting.php
@@ -1,8 +1,8 @@
 <br>
 <div class="container">
 	<div class="row">
-		<div class="col-md-4"></div>
-		<div class="col-md-4">
+		<div class="col-md-3"></div>
+		<div class="col-md-6">
 			<h2>Make A Reservation</h2>
 			<form action="<?php echo base_url();?>Welcome/booking_room" method="post">
 				<div class="form-group">
@@ -40,7 +40,7 @@
 				</div>
 			</form>
 		</div>
-		<div class="col-md-4"></div>
+		<div class="col-md-3"></div>
 	</div>
 </div>
 <?php 
