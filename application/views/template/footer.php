77d77d87dd38e107cd73f06f86e4f02db90c6c3c
PNC\Samreth.SAROEURT <saroeurtsamreth@gmail.com>
2018-05-17 10:58:44 +0700

    hide show button on menu bar from "Samreth.SAROEURT"

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -14,6 +14,17 @@
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/js/tempusdominus-bootstrap-4.min.js"></script>
      <script>
+        
+        $(window).resize(function() {
+          if ($(window).width() < 768) {
+            $("#hide_menu").show();
+          }
+         else {
+           $("#hide_menu").hide();
+         }
+        });
+
+
         $(function(){
             $('#btn-primary').click(function(){
                 var sdate = $('#sdate').val();
