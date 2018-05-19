9761d4b659296649a24f08ddcd02c54c4eb15c05
Chan-Thintha <thintha.chan07@gmail.com>
2018-05-19 16:51:25 +0700

    [GD016] fixed map popup
    refs [GD016]

--- application/views/list_location.php
+++ application/views/list_location.php
@@ -46,13 +46,13 @@
                  </div>
                  <!-- Modal body -->
                  <div class="modal-body">
-                   <!-- <iframe src="<?php  //echo $row->embed_url_map;  ?>" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+                   <!-- <iframe src="<?php  echo $row->embed_url_map;  ?>" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
                    <h3>My Google Maps Demo</h3>
-                   <div id="map"></div>
-                   <!-- Replace the value of the key parameter with your own API key. -->
-                   <script async defer
-                   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAjaIzPJQY_nrDt5zi2ayk1BfeQOHo7Kk&sensor=initMap">
-                   </script>
+                    <div id="map"></div>
+                    <!-- Replace the value of the key parameter with your own API key. -->
+                    <script async defer
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap">
+                    </script>
                  </div>
                  <!-- Modal footer -->
                  <div class="modal-footer">
@@ -112,15 +112,15 @@
 
 
   });
-  function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
+ function initMap() {
+  var uluru = {lat: -23, lng: 1.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
 </script>
