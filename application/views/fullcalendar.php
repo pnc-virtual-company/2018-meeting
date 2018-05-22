758f204e3a1a879066e7180b0f92df8c80748e8f
PNC\Samreth.SAROEURT <saroeurtsamreth@gmail.com>
2018-05-22 16:03:16 +0700

    add on full calendar

--- application/views/fullcalendar.php
+++ application/views/fullcalendar.php
@@ -5,7 +5,7 @@
             <a href="<?php echo base_url(); ?>room/list_room?loc_id=<?php echo $this->input->get('loc_id'); ?>&loc_name=<?php echo $this->input->get('loc_name'); ?>" class="btn btn-primary float-right">
                 <i class="mdi mdi-backspace"></i>&nbsp;Cancel
             </a>&nbsp;
-            <a href="<?php echo base_url(); ?>booking/book_meeting?loc_id=<?php error_reporting(0); echo $this->input->get('loc_id'); ?>&loc_name=<?php echo  $this->input->get('loc_name'); ?>&room_id=<?php echo $this->input->get('room_id'); ?>" class="btn btn-primary float-right">
+            <a href="<?php echo base_url(); ?>booking/book_meeting?loc_id=<?php error_reporting(0); echo $this->input->get('loc_id'); ?>&loc_name=<?php echo  $this->input->get('loc_name'); ?>&room_id=<?php echo $this->input->get('room_id'); ?>&room_name=<?php echo $this->input->get('room_name'); ?>" class="btn btn-primary float-right">
             </i>&nbsp;Make a reservation
         </a>
         <div class="col-md-12">
